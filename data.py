a816b29e959406ad14e0c52c70983bd1d4c9fce3
yiyuezhuo <616271835@163.com>
2016-08-19 20:49:40 +0800

    init

---
+++ data.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 17:27:47 2016
+
+@author: yiyuezhuo
+"""
+
+import tushare as ts
+import pandas as pd
+import os
+import time
+import csv
+
+#df = ts.get_hist_data('600848') #一次性获取全部数据
+
+def read_csv(name):
+    return pd.read_csv( name, 
+                        encoding = 'gbk', 
+                        dtype={'code' : str},
+                        index_col = 0)
+
+
+def use_cache(func):
+    def checked(path, *args, **kwargs):
+        if os.path.isfile(path):
+            print('skip {}'.format(path))
+        else:
+            r = func(path, *args, **kwargs)
+            print('write -> {}'.format(path))
+            return r
+    return checked
+            
+def use_sleep(interval):
+    def _use_sleep(func):
+        def __use_sleep(*args, **kwargs):
+            r = func(*args, **kwargs)
+            time.sleep(interval)
+            return r
+        return __use_sleep
+    return _use_sleep
+
+@use_cache
+@use_sleep(1)
+def code_to_file(path, code):
+    df = ts.get_hist_data(code)
+    df.to_csv(path)
+
+def code_mapping(code_list, 
+                 root = 'hist_data'):
+    for code in code_list:
+        name = code + '.csv'
+        path = os.path.join(root, name)
+        code_to_file(path, code)
+
+def share_download(all_file = '2016-8-19-all.csv',
+                   root = 'hist_data'):
+    df = read_csv(all_file)
+    _,code_list = zip(*df['code'].items())
+    code_mapping(code_list, root = root)
+    
+def index_download(root = 'hist_data'):
+    code_list = ['sh','sz','hs300','sz50','zxb','cyb']
+    # （sh=上证指数 sz=深圳成指 hs300=沪深300指数 sz50=上证50 zxb=中小板 cyb=创业板）
+    code_mapping(code_list, root = root)
+    
+def get_local(code, root = 'hist_data'):
+    path = os.path.join(root,code+'.csv')
+    return read_csv(path)
---
+++ price_test.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 19 18:44:06 2016
+
+@author: yiyuezhuo
+"""
+
+import data
+
+def get_local(code):
+    df = data.get_local(code)
+    df.index = pd.DatetimeIndex(df.index)
+    return df
+
+import numpy as np
+import scipy
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import pandas as pd
+
+
+sh = get_local('sh')
+c600875 = get_local('600875') # 东方电气
+c600283 = get_local('600283') # 钱江水利
+c002674 = get_local('002674') # 兴业科技
+c002509 = get_local('002509') # 天广中茂
+c300033 = get_local('300033') # 同花顺
+c300187 = get_local('300187') # 永清环保
+
+base_rate = 0.025 # 基准/无风险利率，2015前半年的定期存款利率
+
+close_df = pd.DataFrame({'sh'     : sh['close'],
+                         'c600875' : c600875['close'],
+                         'c600283' : c600283['close']})
+                         
+percent_df = close_df.resample('1M').agg(lambda s: s.head(1).ix[0]/s.tail().ix[0]) - 1
+excess_df = percent_df['2015'] - base_rate
